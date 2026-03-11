865392b901af1f8a3174d53b25f10be1774e6ccd
gonos <gonosanna@mail.ru>
2026-03-11 21:46:02 +0300

    add comments

--- apps/admin/src/components/ImageUploadInput.tsx
+++ apps/admin/src/components/ImageUploadInput.tsx
@@ -48,6 +48,9 @@ export function ImageUploadInput({
 
     const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files && event.target.files[0] ? event.target.files[0] : null
+        if (file) {
+            console.log('Выбран файл:', file.name)
+        }
         onChange({
             ...value,
             mode: 'file',
