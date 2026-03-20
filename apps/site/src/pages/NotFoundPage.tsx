47ed54bafabf91db0c935fce7cc9ba3f8ffda285
gonos <gonosanna@mail.ru>
2026-03-21 02:19:30 +0300

    fix notfound page

--- apps/site/src/pages/NotFoundPage.tsx
+++ apps/site/src/pages/NotFoundPage.tsx
@@ -3,13 +3,15 @@ import { PrimaryButtonLink, BackButton } from '../components/LinkButtons'
 
 export default function NotFoundPage() {
     return (
-        <div className="not-found-page">
-            <div className="not-found-page__container">
+        <div className="page-main">
+            <div className="page__container privacy-page__container">
                 <div className="not-found-page__content">
-                    <h1 className="not-found-page__code">404</h1>
+                    <img src="/public/search-loupe.jpg" className="not-found-page__illustration" alt="Страница не найдена" />
+
                     <h2 className="not-found-page__title">Страница не найдена</h2>
-                    <p className="not-found-page__description">
-                        К сожалению, запрашиваемая страница не существует или была перемещена.
+                    <p className="not-found-page__description body-text article-text">
+                        Страница, которую вы искали, не существует. 
+                        Возможно, вы ошиблись при вводе адреса или страница могла быть перемещена
                     </p>
                     <div className="not-found-page__actions">
                         <PrimaryButtonLink href="/">На главную</PrimaryButtonLink>
@@ -18,9 +20,7 @@ export default function NotFoundPage() {
                             Назад
                         </BackButton>
                     </div>
-                </div>
-                <div className="not-found-page__illustration">
-                    <i className="bi bi-exclamation-triangle" />
+                    <h1 className="not-found-page__code">404</h1>
                 </div>
             </div>
         </div>
