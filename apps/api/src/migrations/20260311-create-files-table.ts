e796205b7ca7b2b8dd6f53e36a241afb9818d4dc
gonos <gonosanna@mail.ru>
2026-04-05 19:59:14 +0300

    fix document

--- apps/api/src/migrations/20260311-create-files-table.ts
+++ apps/api/src/migrations/20260311-create-files-table.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CreateFilesTable20260311 implements MigrationInterface {
-  name = 'CreateFilesTable20260311';
+export class CreateFilesTable2026031100000 implements MigrationInterface {
+  name = 'CreateFilesTable2026031100000';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
