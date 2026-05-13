f6dfba7cd4a5b6a6a9baf037b31d0537331530d6
gonos <gonosanna@mail.ru>
2026-05-13 20:39:15 +0300

    fix images education

--- apps/site/src/pages/education/EducationPage.tsx
+++ apps/site/src/pages/education/EducationPage.tsx
@@ -92,12 +92,12 @@ export default function EducationPage() {
                     <a href="/education/srednee-professionalnoe-obrazovanie"
                         rel="nofollow"
                     >
-                        <img className="education-image-banner" src={getFileUrl('/images/education/srednee-600x450.png') || ''} alt="Образование" />
+                        <img className="education-image-banner" src={'/images/education/srednee-600x450.png'} alt="Образование" />
                     </a>
                     <a href="/education/vysshee-professionalnoe-obrazovanie"
                         rel="nofollow"  
                     >
-                        <img className="education-image-banner" src={getFileUrl('/images/education/vysshee-600x450.png') || ''} alt="Образование" />
+                        <img className="education-image-banner" src={'/images/education/vysshee-600x450.png'} alt="Образование" />
                     </a>
                 </ContentSection>
 
@@ -113,11 +113,11 @@ export default function EducationPage() {
                 <ContentSection columns={2}>
                     <a target="_blank" rel="nofollow"
                         href="https://docs.google.com/forms/d/e/1FAIpQLSeHb-dRYqcTY3grMzRbuomG77FSydRo9ErHxIkuyLrUvA9hsg/viewform">
-                        <img className="education-image-banner" src={getFileUrl('/images/education/anketa-vypusknika-600x450.png') || ''} alt="Анкета выпускника" />
+                        <img className="education-image-banner" src={'/images/education/anketa-vypusknika-600x450.png'} alt="Анкета выпускника" />
                     </a>
                     <a target="_blank" rel="nofollow"
                         href="https://docs.google.com/spreadsheets/d/1F78TcG9PUyOxldUDbMuhlWiGI8hMYQJ9SJzQ92cQLN0/edit?gid=0#gid=0">
-                        <img className="education-image-banner" src={getFileUrl('/images/education/baza-vypusknikov-600x450.png') || ''} alt="Таблица выпускников" />
+                        <img className="education-image-banner" src={'/images/education/baza-vypusknikov-600x450.png'} alt="Таблица выпускников" />
                     </a>
                 </ContentSection>
             </div >
