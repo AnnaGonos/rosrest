f769186ff76b35a7f3ecc7f423b8122ae7bda0fd
gonos <gonosanna@mail.ru>
2026-07-05 19:26:11 +0300

    fix charter

--- apps/admin/src/pages/Dashboard/CharterPage.tsx
+++ apps/admin/src/pages/Dashboard/CharterPage.tsx
@@ -113,9 +113,9 @@ export default function CharterPage() {
       fd.append('isPublished', String(isPublished))
 
       if (uploadSource.mode === 'file' && uploadSource.file) {
-        fd.append('pdfFile', uploadSource.file)
+        fd.append('file', uploadSource.file)
       } else if (uploadSource.mode === 'url') {
-        fd.append('pdfUrl', uploadSource.url)
+        fd.append('fileUrl', uploadSource.url)
       }
 
       const res = await fetch(API_ENDPOINTS.DOCUMENTS_CREATE, {
